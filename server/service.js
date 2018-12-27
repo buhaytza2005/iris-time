c0a2b4adb052e8f63bcff08cb0d1a4112d1fced4
buhaytza2005 <44904160+buhaytza2005@users.noreply.github.com>
2018-12-27 00:23:23 +0000

    Update service.js

--- server/service.js
+++ server/service.js
@@ -15,7 +15,7 @@ service.get('/service/:location', (req, res, next) => {
         const location = response.body.results[0].geometry.location
         const timestamp = +moment().format("X");
         
-        request.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=AIzaSyCnSCSMKbFcQKliyoE0rbjhoEHbQ6uPaaY`, (err, response) => {
+        request.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=googleKey`, (err, response) => {
             if(err) {
                 console.log(err);
                 return res.sendStatus(500);
