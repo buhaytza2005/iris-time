502501ce28d12f879357bcd16ae1441fb083f325
buhaytza2005 <44904160+buhaytza2005@users.noreply.github.com>
2018-12-27 01:17:02 +0000

    Added configuration

--- bin/run.js
+++ bin/run.js
@@ -10,11 +10,13 @@ server.on('listening', function() {
     console.log(`IRIS-Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
 
     const announce = () => {
-        request.put(`https://serene-brook-98744.herokuapp.com/service/time/${server.address().port}`, (err, res) => {
+        
+            //this is for online
+        // request.put(`https://serene-brook-98744.herokuapp.com/service/time/${server.address().port}`, (err, res) => {
+        request.put(`http://localhost:5000/service/time/${server.address().port}`, (err, res) => {
             if (err) {
                 //console.log(err);
                 console.log("There was an error connectiong to Iris");
-                console.log(`Just tried connecting to https://serene-brook-98744.herokuapp.com/service/time/${server.address().port}`)
                 return;
             }
             console.log(res.body);
--- server/service.js
+++ server/service.js
@@ -1,10 +1,10 @@
 'use strict';
-
+const config = require("../config");
 const express = require('express');
 const request = require("superagent")
 const service = express();
 const moment = require("moment");
-let googleKey = "Google_Key";
+let googleKey = config.googleApiKey;
 
 service.get('/service/:location', (req, res, next) => {
     request.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${googleKey}`, (err, response) => {
@@ -15,7 +15,7 @@ service.get('/service/:location', (req, res, next) => {
         const location = response.body.results[0].geometry.location
         const timestamp = +moment().format("X");
         
-        request.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=AIzaSyCnSCSMKbFcQKliyoE0rbjhoEHbQ6uPaaY`, (err, response) => {
+        request.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=${googleKey}`, (err, response) => {
             if(err) {
                 console.log(err);
                 return res.sendStatus(500);
