c5d5754cc2ed40fd1f32979692baa810101ba8b0
buhaytza2005 <buhaytza2005@gmail.com>
2018-12-28 02:41:26 +0000

    FINALLY WORKS!!!

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,6 @@ module.exports = {
     ],
     deploy: {
         production: {
-            key: "../iris.pem",
             user: "node",
             host: "52.91.163.214",
             ref: "origin/master",
